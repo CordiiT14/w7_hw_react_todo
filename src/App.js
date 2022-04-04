4b839a22d1b46a99b8e157b1eaa5810954e3109d
CordiiT14 <cordelia.toennies@live.com>
2022-04-04 14:14:26 +0100

    added new todo item form, currently not rendering

--- src/App.js
+++ src/App.js
@@ -6,18 +6,39 @@ function App() {
 
   const [todo, setTodo] = useState(["Food Shopping", "Walk the Dog", "Go to Gym"]);
 
-  const todoNodes = todo.map((todoItem) => {
+  const [newTodo, setNewTodo] = useState[''];
+
+  const todoNodes = todo.map((todoItem, index) => {
     return (
-    <li><span>{todoItem}</span> </li>
+    <li key={index}><span>{todoItem}</span> </li>
   )
   });
 
+  const handleNewTodoItem = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    const copyTodo = [...todo];
+    copyTodo.push(newTodo);
+    setTodo(copyTodo);
+    setNewTodo('');
+  };
+
   return (
     <>
-    <h1>My Todo List:</h1>
-    <ul>
-      {todoNodes}
-    </ul>
+      <h1>My Todo List:</h1>
+
+      <form onSubmit={addTodo}>
+        <input id='new-item' type='text' value={newTodo} onChange={handleNewTodoItem}></input>
+        <input type='subimt' value='Add New Task'></input>
+      </form>
+
+      <ul>
+        {todoNodes}
+      </ul>
+
     </>
   );
 }
